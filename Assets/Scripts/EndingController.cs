84f935b2c18bef4e3d842903c16333534e412326
Ivan Kosov <kosov.vanya@gmail.com>
2024-05-22 12:14:01 -0500

    Added buttons to ending screen

--- Assets/Scripts/EndingController.cs
+++ Assets/Scripts/EndingController.cs
@@ -2,20 +2,38 @@ using Assets.Scripts;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class EndingController : MonoBehaviour
 {
+    private const float waitBeforeOptionsAppear = 11.0f;
+
     public Image whiteFade;
     public Fade fade;
+    public GameObject victoryOverlay;
 
     void Start()
     {
         fade.FadeOut(whiteFade, 1.5f);
+        StartCoroutine(ShowOverlay());
     }
 
-    void Update()
+    public void MainMenuButton()
     {
+        SceneManager.LoadScene("MainMenu");
+    }
 
+    public void QuitButton()
+    {
+        UnityEditor.EditorApplication.isPlaying = false;
+    }
+
+    private IEnumerator ShowOverlay()
+    {
+        yield return new WaitForSeconds(waitBeforeOptionsAppear);
+        victoryOverlay.SetActive(true);
+
+        yield return null;
     }
 }
